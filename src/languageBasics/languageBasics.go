456a9241c367aa8a4786ebae2f5055a55fa0ff81
Leon Yalin <l.yalin@agentvi.com>
2021-04-03 21:24:50 +0300

    finish variable and pointers

--- src/languageBasics/languageBasics.go
+++ src/languageBasics/languageBasics.go
@@ -1,9 +1,8 @@
 package languageBasics
 
 import (
-	"fmt"
-	"log"
-
+	// "fmt"
+	// "log"
 	"github.com/LeonYalinAgentVI/go-learn/src/util"
 )
 
@@ -13,35 +12,33 @@ func gettingStarted() {
 	To run a program, just run 'go run main.go' from the cmd.
 	To compile a program, run 'go build -o hw main.go' from the cmd. That will create a 'hw' binary file. To run it, type './hw' from the cmd.
 	`)
-
-	var msg string = "Leon Yalin" // use variables
-	fmt.Println(sayHello(msg))
-	fmt.Println(returnTwoValues("lulu"))
-
-	// returning multiple values, _ for omitting variables
-	var firstVal, _ = returnTwoValues("lulu")
-	fmt.Println(firstVal)
 }
 
-func sayHello(msg string) string {
-	return msg
-}
-
-func returnTwoValues(msg string) (string, string) {
-	return msg, "lala"
-}
+// var packageVar = 2
 
 func variablesAndFunctions() {
 	util.PrintCmd("Variable & Functions", `
 	- Like in other languages, we have a couple of primitive types:
+
 	var name string = "Leon Yalin"
 	var num int = 0
+
 	func sayHello(mas string) {
 		fmt.Println(msg)
 	}
+
+	var msg string = "Leon Yalin" // use variables
+	fmt.Println(sayHello(msg))
+
 	func returnsTwoValues(msg string) (string, string) {
 		return msg, "lala"
 	}
+
+	// returning multiple values, _ for omitting variables
+	var firstVal, _ = returnTwoValues("lulu")
+	fmt.Println(firstVal)
+
+	We can have a variable on a package scope, like in Javascript.
 	`)
 }
 
@@ -49,7 +46,6 @@ func pointers() {
 	util.PrintCmd("Pointers", `
 	In go, we have pointers and references. References can be used to to get variable memory addresses.
 	Pointers are used to get the value stored in particular memory address (reference).
-	`)
 
 	// Example: change string value using pointers (by reference)
 	var text string = "Hello, Bro"
@@ -57,10 +53,18 @@ func pointers() {
 	var newText *string = &text
 	*newText = "Bye, Broh"
 	log.Println(*newText, text)
+	`)
+}
+
+func typesAndStructs() {
+	util.PrintCmd("Types and structs", `
+	
+	`)
 }
 
 func LanguageBasics() {
 	gettingStarted()
 	variablesAndFunctions()
 	pointers()
+	typesAndStructs()
 }
